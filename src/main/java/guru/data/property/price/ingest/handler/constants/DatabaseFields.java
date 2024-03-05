9ae6f07ab9b04a66a8e7e38310b715875019beb4
Sam Ryecroft <hello@samryecroft.ninja>
2024-03-05 00:34:12 +0000

    chore: Make fields final

--- src/main/java/guru/data/property/price/ingest/handler/constants/DatabaseFields.java
+++ src/main/java/guru/data/property/price/ingest/handler/constants/DatabaseFields.java
@@ -2,15 +2,15 @@ package guru.data.property.price.ingest.handler.constants;
 
 public class DatabaseFields {
 
-  public static String TRANSACTION_FIELD = "transactions";
+  public static final String TRANSACTION_FIELD = "transactions";
 
-  public static String LAST_UPDATED_FIELD = "lastUpdated";
+  public static final String LAST_UPDATED_FIELD = "lastUpdated";
 
-  public static String LATEST_DATA_DATE_FIELD = "latestDataDate";
+  public static final String LATEST_DATA_DATE_FIELD = "latestDataDate";
 
-  public static String PROPERTY_TYPE_FIELD = "propertyType";
+  public static final String PROPERTY_TYPE_FIELD = "propertyType";
 
-  public static String LOCATION_FIELD = "location";
+  public static final String LOCATION_FIELD = "location";
 
-  public static String PROPERTY_ID_FIELD = "_id";
+  public static final String PROPERTY_ID_FIELD = "_id";
 }
